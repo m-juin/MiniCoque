6b8ccd34bbc775aa7bb5ddbf3fcc757bf9508126
Gillian Pasquette <gpasquet@student.42angouleme.fr>
2023-02-02 10:09:45 +0100

    heredoc avec readline

--- srcs/parsing/heredoc_fork.c
+++ srcs/parsing/heredoc_fork.c
@@ -6,7 +6,7 @@
 /*   By: mjuin <mjuin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 10:38:49 by gpasquet          #+#    #+#             */
-/*   Updated: 2023/02/01 15:52:19 by mjuin            ###   ########.fr       */
+/*   Updated: 2023/02/02 10:09:18 by gpasquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,6 @@ static char	*get_limiter(char *token)
 	while (typify(token[i]) == BLANK || token[i] == '<')
 		i++;
 	limiter = (ft_substr(token, i, ft_strlen(token) - i));
-	if (!limiter)
-		return (NULL);
-	limiter = ft_strjoin(limiter, "\n");
 	if (!limiter)
 		return (NULL);
 	return (limiter);
@@ -44,27 +41,26 @@ static int	open_tmp_file(char *path)
 
 static void	prompt_loop(int fd, char *tmp, char *limiter)
 {
-	char *sublimiter;
-
-	sublimiter = ft_substr(limiter, 0, ft_strlen(limiter) - 1);
 	while (tmp[0] == '\0'
 		|| ft_strncmp(tmp, limiter, ft_strlen(limiter)) != 0)
 	{
-		ft_putstr_fd(">", 1);
 		if (tmp[0] != '\0')
 			free(tmp);
-		tmp = get_next_line(0);
+		tmp = readline(" > ");
 		if (tmp == NULL)
-			ft_printf_fd(1, "MiniCoque: warning: here-document delimited by end-of-file (wanted `%s')\n", sublimiter);
+			ft_printf_fd(1,
+				"MiniCoque: warning: here-document delimited by end-of-file (wanted `%s')\n", limiter);
 		if (ft_strncmp(tmp, limiter, ft_strlen(limiter)) != 0)
+		{
+			tmp = ft_strjoin(tmp, "\n");
 			ft_putstr_fd(tmp, fd);
+		}
 	}
-	free(sublimiter);
 	free(limiter);
 	free(tmp);
 }
 
-void	hsighandler()
+void	hsighandler(void)
 {
 	ft_putstr_fd("\n", 1);
 	exit(2);
